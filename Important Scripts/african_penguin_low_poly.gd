25be493ff31580b672bdd8aaa1de619bdbb72780
michaelgerwe <gerwemichael@gmail.com>
2025-11-05 13:08:37 +0000

    Add files via upload

--- Important Scripts/african_penguin_low_poly.gd
+++ Important Scripts/african_penguin_low_poly.gd
@@ -7,6 +7,9 @@ extends CharacterBody3D
 @export var acceleration := 200.0
 @export var rotation_speed := 12.0
 @export var jump_impulse := 24.0
+var blank_material := load("res://3D MODELS/blank.tres")
+var deathbox_material := load("res://3D MODELS/deathbox.tres")
+var breakable_material := load("res://3D MODELS/breakable_wall_proto.tres")
 
 
 @onready var _camera_pivot: Node3D = %CamaraNode
@@ -18,15 +21,27 @@ extends CharacterBody3D
 @onready var _hummingbird: Node3D = $PointOffset/Hummingbird
 @onready var _penguin: Node3D = %PenguinModel
 @onready var _duck: Node3D = $PointOffset/Duck_A1
+@onready var _character: CharacterBody3D = $"."
+@onready var _ray_cast: RayCast3D = $CamaraNode/SpringArm3D/Camera3D/RayCast3D
+
+@onready var checkpoint1: Area3D = $"../Checkpoint"
+@onready var checkpoint2: Area3D = $"../Checkpoint2"
 
 var _camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
 var gravity := -50.0
 var amount_of_jumps = 1
+
 var is_in_penguin := true
 var is_in_hummingbird := false
 var is_in_duck := false
+var is_honking := false
 
+var current_checkpoint : Vector3 = position 
+
+var last_raycast_collider : Node3D = null
+var wall_can_break : bool = false
+var all_broken_walls : Array = []
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
@@ -44,36 +59,39 @@ func _unhandled_input(event: InputEvent) -> void:
 		_camera_input_direction = event.screen_relative * mouse_sensitivity
 
 func _physics_process(delta: float) -> void:
-	_camera_pivot.rotation.x += _camera_input_direction.y * delta
-	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI/12.0, PI / 3.0)
-	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+	if !_duck.is_honking:
+		_camera_pivot.rotation.x += _camera_input_direction.y * delta
+		_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI/12.0, PI / 3.0)
+		_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+		_camera_input_direction = Vector2.ZERO
 	
-	_camera_input_direction = Vector2.ZERO
 	
+	if !_duck.is_honking:
+		if Input.is_action_just_pressed("transform1"):
+			_penguin.visible = true
+			_hummingbird.visible = false
+			_duck.visible = false
+			is_in_penguin = true
+			is_in_hummingbird = false
+			is_in_duck = false
+			gravity = -60.0
+		if Input.is_action_just_pressed("transform3"):
+			_penguin.visible = false
+			_hummingbird.visible = false
+			_duck.visible = true
+			is_in_duck = true
+			is_in_penguin = false
+			is_in_hummingbird = false
+			gravity = -50.0
+		if Input.is_action_just_pressed("transform2"):
+			_penguin.visible = false
+			_hummingbird.visible = true
+			_duck.visible = false
+			is_in_hummingbird = true
+			is_in_penguin = false
+			is_in_duck = false
+			gravity = -3.0
 	
-	
-	if Input.is_action_just_pressed("transform1"):
-		_penguin.visible = true
-		_hummingbird.visible = false
-		_duck.visible = false
-		is_in_penguin = true
-		is_in_hummingbird = false
-		gravity = -60.0
-	if Input.is_action_just_pressed("transform3"):
-		_penguin.visible = false
-		_hummingbird.visible = false
-		_duck.visible = true
-		is_in_duck = true
-		is_in_penguin = false
-		is_in_hummingbird = false
-		gravity = -50.0
-	if Input.is_action_just_pressed("transform2"):
-		_penguin.visible = false
-		_hummingbird.visible = true
-		_duck.visible = false
-		is_in_hummingbird = true
-		is_in_penguin = false
-		gravity = -3.0
 	
 	
 	var raw_input := Input.get_vector("left", "right", "forward", "back")
@@ -81,12 +99,22 @@ func _physics_process(delta: float) -> void:
 	var right := _camera.global_basis.z
 	var sprinting := Input.is_action_pressed("sprint")
 	if is_in_duck:
-		if sprinting:
+		if Input.is_action_just_pressed("interact") and _duck.is_honking == false and is_in_duck:
+			_duck.honk()
+			if last_raycast_collider != null and wall_can_break:
+				last_raycast_collider.visible = false
+				last_raycast_collider.use_collision = false
+				all_broken_walls.append(last_raycast_collider)
+		elif sprinting:
 			move_speed = 40.0
 			acceleration = 200.0
 		else:
 			move_speed = 20.0
 			acceleration = 200.0
+		if _duck.is_honking:
+			move_speed = 0
+
+
 	if is_in_penguin:
 		if sprinting:
 			move_speed = 25.0
@@ -105,48 +133,81 @@ func _physics_process(delta: float) -> void:
 	var y_velocity := velocity.y
 	velocity.y = 0.0
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	
 	if is_in_duck:
 		velocity.y = y_velocity + gravity * delta
+		if _ray_cast.is_colliding():
+			last_raycast_collider = _ray_cast.get_collider()
+			_ray_cast.get_collider().set_material(breakable_material)
+			wall_can_break = true
+		elif last_raycast_collider != null:
+			last_raycast_collider.set_material(blank_material)
+			wall_can_break = false
+		
+	
 	if is_in_penguin:
 		velocity.y = y_velocity + gravity * delta
+		if last_raycast_collider != null:
+			last_raycast_collider.set_material(blank_material)
+			wall_can_break = false
 		var is_starting_jump := Input.is_action_just_pressed("jump")
 		if is_starting_jump and amount_of_jumps > 0:
 			if !is_on_floor():
 				_jump_particles.emitJump()
 			velocity.y = jump_impulse
 			amount_of_jumps = amount_of_jumps - 1
+	
 	if is_in_hummingbird:
 		velocity.y = gravity
-	
+		if last_raycast_collider != null:
+			last_raycast_collider.set_material(blank_material)
+			wall_can_break = false
 	
 	
 	move_and_slide()
 	
-	
-	if move_direction.length() > 0.2:
-		_last_movement_direction = move_direction
-		if sprinting:
-			_animator_tree["parameters/conditions/is_idle"] = false
-			_animator_tree["parameters/conditions/is_walking"] = false
-			_animator_tree["parameters/conditions/is_sprint"] = true
-			_duck.is_running()
-			
+	if !_duck.is_honking:
+		var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+		_skin.global_rotation.y = lerp_angle(_skin.global_rotation.y, target_angle, rotation_speed * delta)
+		if move_direction.length() > 0.2:
+			_last_movement_direction = move_direction
+			if sprinting:
+				_animator_tree["parameters/conditions/is_idle"] = false
+				_animator_tree["parameters/conditions/is_walking"] = false
+				_animator_tree["parameters/conditions/is_sprint"] = true
+				_duck.is_running()
+			else:
+				_animator_tree["parameters/conditions/is_idle"] = false
+				_animator_tree["parameters/conditions/is_walking"] = true
+				_animator_tree["parameters/conditions/is_sprint"] = false
+				_duck.is_walking()
 		else:
-			_animator_tree["parameters/conditions/is_idle"] = false
-			_animator_tree["parameters/conditions/is_walking"] = true
+			_animator_tree["parameters/conditions/is_walking"] = false
+			_animator_tree["parameters/conditions/is_idle"] = true
 			_animator_tree["parameters/conditions/is_sprint"] = false
-			_duck.is_walking()
-			
-	else:
-		_animator_tree["parameters/conditions/is_walking"] = false
-		_animator_tree["parameters/conditions/is_idle"] = true
-		_animator_tree["parameters/conditions/is_sprint"] = false
-		_duck.is_idle()
+			_duck.is_idle()
 	
-	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-	_skin.global_rotation.y = lerp_angle(_skin.global_rotation.y, target_angle, rotation_speed * delta)
+	
 	if is_on_floor():
 		amount_of_jumps = 2
-	
-	
-	
+
+
+
+
+func _on_checkpoint_body_entered(_character) -> void:
+	current_checkpoint = checkpoint1.position # Replace with function body.
+
+
+func _on_deathbox_body_entered(_character) -> void:
+	position = current_checkpoint
+	for i in all_broken_walls.size():
+		all_broken_walls[i].visible = true
+		all_broken_walls[i].set_material(blank_material)
+
+
+func _on_checkpoint_2_body_entered(_character) -> void:
+	current_checkpoint = checkpoint2.position # Replace with function body.
+
+
+func _on_teleporter_body_entered(_character) -> void:
+	position = checkpoint1.position # Replace with function body.
--- Important Scripts/duck_a_1.gd
+++ Important Scripts/duck_a_1.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @onready var _animator_tree: AnimationTree = $AnimationTree
+@onready var _timer: Timer = $CooldownTimer
+@onready var _ray_cast :RayCast3D = $RayCast3D
 
+var is_honking := false
 func is_walking():
 	_animator_tree["parameters/conditions/is_walking"] = true
 	_animator_tree["parameters/conditions/is_idle"] = false
@@ -12,7 +15,10 @@ func is_running():
 	_animator_tree["parameters/conditions/is_idle"] = false
 	_animator_tree["parameters/conditions/is_honk"] = false
 	_animator_tree["parameters/conditions/is_running"] = true
-func is_honk():
+func honk():
+	is_honking = true
+	print("started honking")
+	_timer.start()
 	_animator_tree["parameters/conditions/is_walking"] = false
 	_animator_tree["parameters/conditions/is_idle"] = false
 	_animator_tree["parameters/conditions/is_honk"] = true
@@ -22,3 +28,9 @@ func is_idle():
 	_animator_tree["parameters/conditions/is_idle"] = true
 	_animator_tree["parameters/conditions/is_honk"] = false
 	_animator_tree["parameters/conditions/is_running"] = false
+
+
+
+func _on_cooldown_timer_timeout() -> void:
+	is_honking = false
+	print("Stopped Honking")
